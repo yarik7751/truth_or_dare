b504c82e3a89cd73d4157eae149b869cbed2c420
yarik7751 <yaroslav.levshunov2@gmail.com>
2019-04-03 22:48:56 +0300

    beginning

---
+++ app/src/main/java/by/yarik/truth_or_dare/MainActivity.kt
@@ -0,0 +1,12 @@
+package by.yarik.truth_or_dare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
